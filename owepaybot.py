2033acb51f6dc83c2c27b3e20e6d8e1da01bb312
jianoway <77208762+jianoway@users.noreply.github.com>
2021-05-24 18:24:08 +0800

    Inline Keyboard V0.1

--- owepaybot.py
+++ owepaybot.py
@@ -1,7 +1,8 @@
 import logging
 import os
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext, Filters
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -20,6 +21,18 @@ def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
+def inlineKeyboardTest(update, context):
+    keyboard = [
+        [
+            InlineKeyboardButton("Option 1", callback_data='1'),
+            InlineKeyboardButton("Option 2", callback_data='2'),
+        ],
+        [InlineKeyboardButton("Option 3", callback_data='3')],
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.message.reply_text('Please choose:', reply_markup=reply_markup)
 
 def main():
     """Start the bot."""
@@ -34,7 +47,7 @@ def main():
 
     # on noncommand i.e message - echo the message on Telegram
 
-    
+    dp.add_handler(CommandHandler("testInline", inlineKeyboardTest))    
 
     # log all errors
     dp.add_error_handler(error)
