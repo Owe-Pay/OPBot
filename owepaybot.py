57de1d57be2212066b63467a32cc9fe7b9e1883c
jianoway <77208762+jianoway@users.noreply.github.com>
2021-05-24 14:29:44 +0800

    Initial push to Heroku

---
+++ owepaybot.py
@@ -0,0 +1,47 @@
+import logging
+import os
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+PORT = int(os.environ.get('PORT', '8443'))
+
+TOKEN = os.environ(API_TOKEN)
+
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
+
+
+def main():
+    """Start the bot."""
+    updater = Updater(
+        TOKEN, use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    updater.start_webhook(listen="0.0.0.0",
+                          port=PORT,
+                          url_path=TOKEN,
+                          webhook_url="https://owepaybot.herokuapp.com/" + TOKEN)
+
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
