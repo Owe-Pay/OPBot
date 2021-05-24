2f80410db6ada77eddba8a73c56e7541cf3170fb
jianoway <77208762+jianoway@users.noreply.github.com>
2021-05-24 14:43:15 +0800

    Fix error command

--- owepaybot.py
+++ owepaybot.py
@@ -16,8 +16,8 @@ def start(update, context):
     """Send a message when the command /start is issued."""
     update.message.reply_text('Hi!')
 
-    def error(update, context):
-        """Log Errors caused by Updates."""
+def error(update, context):
+    """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 
