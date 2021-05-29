afac36ab96b2451c2d2aaccfa5510e3bc57c8737
joeltohyiliang <joeltohyiliang99@gmail.com>
2021-05-30 00:26:44 +0800

    add functions for the sql table

---
+++ bot_sql_integration.py
@@ -0,0 +1,101 @@
+import mysql.connector
+from tabulate import tabulate
+import datetime
+import time
+
+##connect to mysql database##
+mysqldb = mysql.connector.connect(
+    host='localhost', user='root', passwd='', db='bot',
+    auth_plugin = 'mysql_native_password')
+
+mycursor = mysqldb.cursor()
+
+def massDelete(table):  # Do note that this mass delete removes everything from a table, but it does not reset the auto-increment value (drop table to reset it)
+    mycursor.execute("DELETE FROM " + table)
+    mysqldb.commit()
+    print('Records updated successfully! Your table is now empty')
+
+# massDelete("bot.transactions")
+#userr is stored as {'id': 497722299 ,'username': 'jianowa',"notifiable": boolean 1}
+normalUser1 = ('497722299', 'bear', 1)
+normalUser2 = ('487722299', 'apple',0)
+normalUser3 = ('477722299', 'donkey',1)
+
+
+def addingUsers(input):
+    try:
+        sql = "INSERT into bot.users(UserID, UserName, notifiable) VALUES (%s, %s, %s)"
+        val = input
+        mycursor.execute(sql,val)
+        mysqldb.commit()
+        print('Records inserted successfully!')
+    except:
+        print("entry already in database")
+        mysqldb.rollback()
+    mysqldb.close()
+
+#addingUsers(normalUser2)
+
+def display_Users():
+    mycursor.execute("SELECT * from bot.users")
+    result = mycursor.fetchall()
+    print(tabulate(result, headers=[
+          "UserID", "UserName", "notifiable"]))
+#display_Users
+
+# timing = datetime.datetime.now()
+# dt_obj = datetime.datetime.strptime(str(timing), '%Y-%m-%d %H:%M:%S.%f')
+# print(dt_obj)
+
+transaction1 =('1288299','123213124','2021-05-29', 10, '497722299', '487722299')
+# transaction2=
+### transaction stored as (transactionid, OrderID, date, AmountOwed, UserID_Creditor, User_id_debitor)
+def addTransaction(input):
+    try:
+        sql = "INSERT into bot.transactions(transaction_id, OrderID, date, AmountOwed, UserID_Creditor, UserID_Debitor) VALUES (%s, %s, %s, %s, %s, %s)"
+        val = input
+        mycursor.execute(sql,val)
+        mysqldb.commit()
+        print('Records inserted successfully!')
+    except:
+        print("entry already in database")
+        mysqldb.rollback()
+    mysqldb.close()
+
+# addTransaction(transaction1)
+
+
+#################### orders are OrderID, GroupID ###########################
+order1 = ('1289923','241414')
+
+def addOrder(input):
+    try:
+        sql = "INSERT into bot.orders(OrderID, GroupID) VALUES (%s, %s)"
+        val = input
+        mycursor.execute(sql,val)
+        mysqldb.commit()
+        print('Records inserted successfully!')
+    except:
+        print("entry already in database")
+        mysqldb.rollback()
+    mysqldb.close()
+
+# addOrder(order1)
+
+
+########################## groups are groupid, Number_of_members ###############
+group1=('241414',3)
+
+def addGroup(input):
+    try:
+        sql = "INSERT into bot.groups(GroupID, Number_of_members) VALUES (%s, %s)"
+        val = input
+        mycursor.execute(sql,val)
+        mysqldb.commit()
+        print('Records inserted successfully!')
+    except:
+        print("entry already in database")
+        mysqldb.rollback()
+    mysqldb.close()
+
+addGroup(group1)
--- botsql_1.sql
+++ botsql_1.sql
@@ -4,25 +4,39 @@ CREATE DATABASE bot;
 -- ------------------------------------------- Create the Tables / Collections --------------------------------------------------
 -- ------------------------------------------------------------------------------------------------------------------------------
 CREATE TABLE `bot`.`admins` (
-  `UserID` int NOT NULL AUTO_INCREMENT,
+  `UserID` int NOT NULL ,
   `UserName` varchar(45) NOT NULL,
   PRIMARY KEY (`UserID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `bot`.`users` (
-  `UserID` int NOT NULL AUTO_INCREMENT,
+  `UserID` int NOT NULL ,
   `UserName` varchar(45) NOT NULL,
-  `group` varchar(45) NOT NULL,
+  `notifiable` BOOLEAN ,
+  `date` DATETIME DEFAULT NULL,
   PRIMARY KEY (`UserID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `bot`.`message` (
-  `UserID` int NOT NULL AUTO_INCREMENT,
-  `chatid` varchar(45) NOT NULL,
-  `date` datetime DEFAULT NULL,
-  `type` varchar(45) NOT NULL,
-  `first_name` text NOT NULL,
-  PRIMARY KEY (`UserID`)
+CREATE TABLE `bot`.`transactions` (
+	`transaction_id` INT NOT NULL AUTO_INCREMENT,
+    `OrderID` INT NOT NULL,
+    `date` DATETIME DEFAULT NULL,
+    `AmountOwed` DOUBLE NOT NULL DEFAULT '0',
+    `UserID_Creditor` INT NOT NULL,
+    `UserID_Debitor` INT NOT NULL,
+    PRIMARY KEY (`transaction_id`)
+)  ENGINE=INNODB AUTO_INCREMENT=797 DEFAULT CHARSET=UTF8MB4 COLLATE = UTF8MB4_0900_AI_CI;
+
+CREATE TABLE `bot`.`Orders` (
+  `OrderID` int NOT NULL AUTO_INCREMENT,
+  `GroupID` double NOT NULL DEFAULT '0',
+  PRIMARY KEY (`OrderID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `bot`.`Groups` (
+  `GroupID` varchar(45) NOT NULL,
+  `Number_of_members` double NOT NULL DEFAULT '0',
+  PRIMARY KEY (`GroupID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `bot`.`Owed_Amount` (
@@ -30,6 +44,7 @@ CREATE TABLE `bot`.`Owed_Amount` (
   `AmountOwed` double NOT NULL DEFAULT '0',
   PRIMARY KEY (`UserID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 
 CREATE TABLE `bot`.`Payment` (
   `PaymentID` int NOT NULL AUTO_INCREMENT,
