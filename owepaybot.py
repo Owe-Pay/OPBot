e7768cb16b34a88c6da7a46d5646d9c6a826ec6d
jianoway <77208762+jianoway@users.noreply.github.com>
2021-06-15 02:59:12 +0800

    Comment out polling

--- owepaybot.py
+++ owepaybot.py
@@ -385,11 +385,11 @@ def main():
     # log all errors
     dp.add_error_handler(error)
 
-    # updater.start_webhook(listen="0.0.0.0",
-    #                       port=PORT,
-    #                       url_path=TOKEN,
-    #                       webhook_url="https://owepaybot.herokuapp.com/" + TOKEN)
-    updater.start_polling()
+    updater.start_webhook(listen="0.0.0.0",
+                          port=PORT,
+                          url_path=TOKEN,
+                          webhook_url="https://owepaybot.herokuapp.com/" + TOKEN)
+    # updater.start_polling()
     updater.idle()
 
 if __name__ == '__main__':
